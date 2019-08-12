5c2d6215233175f127fcab18a0b0ea5595f5e05f
ozge sanli <ozgezeynep08@gmail.com>
2019-08-12 16:49:52 +0200

    session added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,21 +4,22 @@ require 'nokogiri'
 class GamesController < ApplicationController
   def new
     @array = calculate
+    session[:count] = 0
   end
 
   def score
-    array = params[:array]
+    array = params[:array].downcase
     @created_word = params[:word]
+    session[:count] = @created_word.split('').count
+    @calcul = session[:count] * session[:count]
     @user = {}
     url = "https://wagon-dictionary.herokuapp.com/#{@created_word}"
     response = open(url).read
     @data = JSON.parse(response)
-
     @exist = true
-    @data["word"].split.each do |letter|
+    @data["word"].split('').each do |letter|
       @exist = false unless array.include?(letter)
     end
-
     if @exist == true && @data["found"] == true
       @result = "Conguratulations!#{@created_word} is an English word!"
     elsif @exist == false
@@ -32,5 +33,9 @@ class GamesController < ApplicationController
 
   def calculate
     (0..10).map { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def create
+    session[:user_id] = "m/ueqGM7N8x8wJSEEAvWI8T/60dlGKH9Ad0NkpB7mgRVb2PlzQUh5kIU76yG7kJW1P/uBGKB7IOC/w/sMBIwxw=="
   end
 end
